545cb15867d6892fde3ac644d7c72a619393c90c
Tanya Aggarwal <tanya.aggarwal106@gmail.com>
2026-02-27 21:37:42 +0530

    Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/player/:courseId' element={<Player />}/>
         <Route path='/loading/:path' element={<Loading />}/>
         <Route path='/educator' element={<Educator/>}>
-            <Route path='educator' element={<Dashboard/>}/>
+            <Route path='dashboard' element={<Dashboard/>}/>
             <Route path='add-course' element={<AddCourse/>}/>
             <Route path='my-courses' element={<MyCourses/>}/>
             <Route path='student-enrolled' element={<StudentsEnrolled/>}/>
